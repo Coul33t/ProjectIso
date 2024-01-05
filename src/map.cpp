21e08034513ba30bde51dc69c8ac816d5eadff98
Quentin Couland <Coulis1990@gmail.com>
2024-01-05 19:04:42 +0100

    :memo: Slightly different map generation

--- src/map.cpp
+++ src/map.cpp
@@ -26,15 +26,34 @@ void Map::make_test_map() {
 
     for (size_t y = 0; y < size.h; y++) {
         for (size_t x = 0; x < size.w; x++) {
-            int z = Random::get_int(-1, 0);
-            if(z >= 0)
-                tiles.emplace_back(Tile("grassfull", x, y, z));
-            else if (z == -1)
-                tiles.emplace_back(Tile("dirtgrassfull", x, y, z));
-            else if (z == -2)
-                tiles.emplace_back(Tile("dirtfull", x, y, z));
-            else if (z == -3)
-                tiles.emplace_back(Tile("waterfull", x, y, z));
+            // Debug tile
+            if (x == 0 && y == 0) 
+                tiles.emplace_back(Tile(("waterfull"), x, y, 0));
+
+            // Debug tile
+            else if (x == 1 && y == 0) {
+                tiles.emplace_back(Tile(("stonefull"), x, y, 0));
+            }
+
+            else {
+                int z = Random::get_int(-2, 0);
+                if(z >= 0) {
+                    tiles.emplace_back(Tile("grassfull", x, y, z));
+                }
+                
+                else if (z == -1) {
+                    tiles.emplace_back(Tile("dirtgrassfull", x, y, z));
+                }
+
+                else if (z == -2) {
+                    tiles.emplace_back(Tile("dirtfull", x, y, z));
+                }
+
+                else if (z == -3) {
+                    tiles.emplace_back(Tile("waterfull", x, y, z));
+                }
+            }
+            
         }
     }
 }
