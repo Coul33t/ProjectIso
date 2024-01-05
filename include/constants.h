6573af2d7a4a673e7eb72ffa82183700e95e9c59
Quentin Couland <Coulis1990@gmail.com>
2024-01-05 18:48:49 +0100

    :memo: Added some constants

--- include/constants.h
+++ include/constants.h
@@ -6,10 +6,16 @@
 #define PROJECTISO_CONSTANTS_H
 
 namespace Constants {
-    static const int TILE_W = 32;
-    static const int TILE_H = 32;
+    static const int WINDOW_SIZE_W = 1280;
+    static const int WINDOW_SIZE_H = 960; 
+
     static const int TARGET_FPS = 60;
     static const int TARGET_FRAME_DELAY_MS = 1000 * (1/TARGET_FPS);
+
+    static const int TILE_W = 32;
+    static const int TILE_H = 32;
+
+    
 }
 
 
