0a63c532260e65e049a1aeaae685df70e9abf913
Quentin Couland <Coulis1990@gmail.com>
2024-01-06 16:45:50 +0100

    :sparkles: Added Sprite struct

---
+++ include/sprite.h
@@ -0,0 +1,22 @@
+//
+// Created by couland-q on 05/01/2024.
+//
+
+#ifndef PROJECTISO_SPRITE_H
+#define PROJECTISO_SPRITE_H
+
+#include <string>
+
+#include <SDL.h>
+
+#include "types.h"
+
+struct Sprite {
+    int idx;
+    mVec2<int> pos;
+    SDL_Rect rect;
+    std::string name;
+    std::string spritesheet_name;
+};
+
+#endif //PROJECTISO_SPRITE_H
