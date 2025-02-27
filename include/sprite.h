ca070a2c5cc87aa4e8340e1cafd0114663347607
Quentin Couland <Coulis1990@gmail.com>
2025-02-27 18:58:25 +0100

    :building_construction: Split Sprite into StaticSprite and AnimatedSprite

--- include/sprite.h
+++ include/sprite.h
@@ -11,12 +11,62 @@
 
 #include "types.h"
 
-struct Sprite {
-    int idx;
-    mVec2<int> pos;
-    SDL_Rect rect;
+/**
+ * Basic sprite, with just the name and the spritesheet name. Shouldn't be instanciated (kinda useless as is).
+*/
+struct BasicSprite {
+    // Name of the sprite
     std::string name;
+    // Name of the spritesheet from where the sprite is taken
     std::string spritesheet_name;
+};
+
+/**
+ * A sprite that isn't animated.
+*/
+struct StaticSprite: BasicSprite {
+    // Index of the sprite (from 0 to k, with k = width * height)
+    int idx;
+    // Position of the top left corner of the sprite in the spritesheet
+    mVec2<int> pos;
+    // SDL Rectangle of the sprite in the spritesheet
+    SDL_Rect rect;
+
+    StaticSprite() {
+        idx = -1;
+        pos.x = -1;
+        pos.y = -1;
+        rect.x = -1;
+        rect.y = -1;
+        rect.w = -1;
+        rect.h = -1;
+    }
+
+    StaticSprite(int i, int j, const TileSetInfo& tileset_info, 
+                 const std::string& name, const std::string& spritesheet_name) {
+        this->idx = (i*tileset_info.nb_tiles.x) + j;
+        this->pos.x = j;
+        this->pos.y = i;
+        this->rect = SDL_Rect{i*tileset_info.tile_size.h, 
+                              j*tileset_info.tile_size.w,
+                              tileset_info.tile_size.w,
+                              tileset_info.tile_size.h};
+        this->name = name;
+        this->spritesheet_name = spritesheet_name;
+    }
+};
+
+/**
+ * A sprite that is animated.
+*/
+struct AnimatedSprite: BasicSprite {
+    int current_sprite_idx;
+    
+    AnimatedSprite() {
+
+    }
+
+    
 };
 
 #endif //PROJECTISO_SPRITE_H
