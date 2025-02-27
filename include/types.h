13761a26e4ca43383106786c9abdc9e44e269573
Quentin Couland <Coulis1990@gmail.com>
2025-02-27 18:59:35 +0100

    :truck: Moved TileSetInfo into types

--- include/types.h
+++ include/types.h
@@ -27,4 +27,29 @@ struct mVec3
     };
 };
 
+struct TileSetInfo {
+    mVec2<int> tile_size;
+    mVec2<int> offsets;
+    mVec2<int> size;
+    mVec2<size_t> nb_tiles;
+
+    TileSetInfo() {
+        tile_size.w = -1;
+        tile_size.h = -1;
+    }
+
+    TileSetInfo(int tile_width, int tile_height) {
+        tile_size.w = tile_width;
+        tile_size.h = tile_height;
+    }
+
+    TileSetInfo(int tile_width, int tile_height,
+                int horizontal_offset, int vertical_offset) {
+        tile_size.w = tile_width;
+        tile_size.h = tile_height;
+        offsets.x = horizontal_offset;
+        offsets.y = vertical_offset;
+    }
+};
+
 #endif //PROJECTISO_TYPES_H
