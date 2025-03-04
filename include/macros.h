b688e36cf4787956c82bfecee9f4cd182b54f921
Quentin Couland <couland-q@ird222-07-l.etu.polytech.univ-nantes.prive>
2025-03-04 15:33:37 +0100

    :truck: Moving macros to a standalone file

---
+++ include/macros.h
@@ -0,0 +1,4 @@
+#define FULL_LOCATION "[" << __FILE__ << " | " << __FUNCTION__ << " | l." << __LINE__ << "]\n"
+#define FILE_NAME "[" << __FILE__ << "]"
+#define FUNCTION_NAME "[" << __FUNCTION__ << "]"
+#define LINE "l." << __LINE__
